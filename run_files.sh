f2288e4ddf3ebd78b8ba75939a08df0d41584aff
Phạm Hồng Thái <55950535+phamthainb@users.noreply.github.com>
2023-03-12 15:46:23 +0700

    Update run_files.sh

--- run_files.sh
+++ run_files.sh
@@ -5,13 +5,14 @@
 # Loop through each argument passed in
 for file in "$@"
 do
-    # Check if the file exists
-    if [ -f "$file" ]; then
+    # Check if the file exists and has a .sh extension
+    if [ -f "$file" ] && [ "${file##*.}" = "sh" ]; then
         # Make the file executable
         chmod +x "$file"
         # Execute the file
         "./$file"
     else
-        echo "File $file not found"
+        echo "File $file not found or not a shell script"
     fi
 done
+
