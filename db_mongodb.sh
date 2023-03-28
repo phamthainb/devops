0e278bf8e5b84b1800bac0448e06d39529ad2832
Phạm Hồng Thái <55950535+phamthainb@users.noreply.github.com>
2023-03-29 01:50:52 +0700

    Update db_mongodb.sh

--- db_mongodb.sh
+++ db_mongodb.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 set -e
 
-# check update
-apt update
-apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
-
-# Import the MongoDB public key
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-
-# Create a MongoDB list file
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
-
-# Update the package list and install MongoDB
+# folow this link: https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/#install-mongodb-community-edition
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
