5ba26a68810199c55538681ad6ade79c74e28f44
Phạm Hồng Thái <55950535+phamthainb@users.noreply.github.com>
2023-03-29 01:38:43 +0700

    Create db_mongodb.sh

---
+++ db_mongodb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Import the MongoDB public key
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+
+# Create a MongoDB list file
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+
+# Update the package list and install MongoDB
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+# Start and enable MongoDB
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+# Check the status of MongoDB
+sudo systemctl status mongod
