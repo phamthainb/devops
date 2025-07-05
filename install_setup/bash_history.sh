dc2f96fac30371f5f95f705e4f5b47f5f4ee3179
Phạm Hồng Thái <55950535+phamthainb@users.noreply.github.com>
2025-07-05 21:48:26 +0700

    Create bash_history.sh

---
+++ install_setup/bash_history.sh
@@ -0,0 +1,9 @@
+echo '# === Unlimited Bash History with Timestamp ===' >> ~/.bashrc
+echo 'export HISTSIZE=' >> ~/.bashrc
+echo 'export HISTFILESIZE=' >> ~/.bashrc
+echo 'export HISTTIMEFORMAT="%F %T "' >> ~/.bashrc
+echo 'export HISTCONTROL=ignoredups:ignorespace' >> ~/.bashrc
+echo 'export PROMPT_COMMAND="history -a; history -n"' >> ~/.bashrc
+echo 'shopt -s histappend' >> ~/.bashrc
+echo '# =============================================' >> ~/.bashrc
+echo 'echo "[ℹ️] Unlimited history + timestamp enabled. Run: source ~/.bashrc"'
