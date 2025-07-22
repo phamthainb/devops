a0fa1902f8df45a500c449e0ff89180ae0b37726
phamthai072 <153043445+phamthai072@users.noreply.github.com>
2025-07-22 09:31:04 +0700

    Update k8s-node-init.sh

--- k8s/k8s-node-init.sh
+++ k8s/k8s-node-init.sh
@@ -34,8 +34,9 @@ systemctl restart containerd && systemctl enable containerd
 
 # Cài kubeadm, kubelet, kubectl ->> v1.30
 apt install -y apt-transport-https ca-certificates curl
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
 
 apt update
